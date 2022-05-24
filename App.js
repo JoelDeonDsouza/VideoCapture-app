ccb63298c2b1a6bd37fea0c6bf658a6096ca2c8d
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-05-24 15:23:49 +0200

    Initial Vedio rendering processed app

---
+++ App.js
@@ -0,0 +1,185 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Dimensions, Pressable } from "react-native";
+import { Camera } from "expo-camera";
+import { Video } from "expo-av";
+
+var { height, width } = Dimensions.get("window");
+
+export default function App() {
+  const [audioAccess, setAudioAccess] = useState(null);
+  const [cameraAccess, setCameraAccess] = useState(null);
+  const [cam, setCam] = useState(null);
+  const [startRecord, setSatrtRecord] = useState(null);
+  const [camType, setCamType] = useState(Camera.Constants.Type.back);
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+
+  useEffect(() => {
+    (async () => {
+      const cameraSetting = await Camera.requestCameraPermissionsAsync();
+      setCameraAccess(cameraSetting.status === "granted");
+
+      const audioSetting = await Camera.requestMicrophonePermissionsAsync();
+      setAudioAccess(audioSetting.status === "Denied");
+    })();
+  }, []);
+
+  const takeVideo = async () => {
+    if (cam) {
+      const data = await cam.startRecordAsync({
+        maxDuration: 45,
+      });
+      setSatrtRecord(data.uri);
+      console.log(data.uri);
+    }
+  };
+
+  const stopVideo = async () => {
+    cam.stopRecording();
+  };
+
+  //Error Granting access
+  if (cameraAccess === false) {
+    return <Text style={styles.errorText}>Error Accessing to the Camera</Text>;
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={styles.camContainer}>
+        <Camera
+          ref={(ref) => setCam(ref)}
+          style={styles.fixedRatio}
+          type={camType}
+          ratio={"5"}
+        />
+      </View>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: startRecord,
+        }}
+        useNativeControls
+        resizMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+      />
+      <View style={styles.btn}>
+        <View style={styles.play}>
+          <Pressable
+          // onPress={() =>
+          //   status.isPlaying
+          //     ? video.current.pauseAsync()
+          //     : video.current.playAsync()
+          // }
+          >
+            <Text style={styles.textBTN1}>Play</Text>
+          </Pressable>
+        </View>
+      </View>
+
+      <View style={styles.btns}>
+        <View style={styles.btnRecord}>
+          <Pressable onPress={() => takeVideo()}>
+            <Text style={styles.textBTN}>Start</Text>
+          </Pressable>
+        </View>
+
+        <View style={styles.btnStop}>
+          <Pressable onPress={() => stopVideo()}>
+            <Text style={styles.textBTN}>Stop</Text>
+          </Pressable>
+        </View>
+      </View>
+
+      <View style={styles.btn}>
+        <View style={styles.save}>
+          <Pressable>
+            <Text style={styles.textBTN1}>Send</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  camContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 0.5,
+  },
+  video: {
+    alignSelf: "center",
+    width: 350,
+    height: 250,
+  },
+  btn: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  play: {
+    width: 65,
+    height: 65,
+    backgroundColor: "#F9D923",
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: "#187498",
+  },
+  save: {
+    width: 65,
+    height: 65,
+    backgroundColor: "#4D96FF",
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: "#187498",
+    marginBottom: height / 90,
+  },
+  btns: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: height / 90,
+  },
+
+  btnRecord: {
+    width: 80,
+    height: 80,
+    backgroundColor: "#00FFAB",
+    borderRadius: 100,
+    marginLeft: width / 20,
+    borderWidth: 2,
+    borderColor: "#187498",
+  },
+  btnStop: {
+    width: 80,
+    height: 80,
+    backgroundColor: "#EB5353",
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: "#187498",
+    marginRight: width / 20,
+  },
+  textBTN: {
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+    alignSelf: "center",
+    marginTop: height / 30,
+  },
+  textBTN1: {
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold",
+    alignSelf: "center",
+    marginTop: height / 40,
+  },
+
+  errorText: {
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+});
